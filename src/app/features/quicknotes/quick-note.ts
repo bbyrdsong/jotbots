486b14efed1fbabed8ca18ee45a4deb00b13e920
Brian Byrdsong <Brian Byrdsong>
2017-01-04 00:07:26 -0500

    ts linting

--- src/app/features/quicknotes/quick-note.ts
+++ src/app/features/quicknotes/quick-note.ts
@@ -3,7 +3,7 @@ import {JotBot} from '../../shared/jotbot';
 export class QuickNote extends JotBot {
     note: string;
 
-    constructor(id:number, note: string) {
+    constructor(id: number, note: string) {
         super(id, 'quick-note');
 
         this.note = note;
