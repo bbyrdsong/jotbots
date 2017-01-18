89b3b68097a53efeab9d70c3fedf42ab597cd8f4
Brian Byrdsong <Brian Byrdsong>
2017-01-18 01:05:56 -0500

    complete timer for task

--- src/app/features/tasks/task.component.ts
+++ src/app/features/tasks/task.component.ts
@@ -21,17 +21,13 @@ export class TaskComponent implements OnInit, OnDestroy {
     private timer;
     private period: number = 1000;
     private sub: Subscription;
-    private startingTime: number;
     private timeDiff: number;
 
     ngOnInit(): void {
-        let dt1 = new Date(this.task.dueDate);
-        let dt2 = new Date();
-        let utc1 = Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate());
-        let utc2 = Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate());
+        let dt1 = new Date(this.task.dueDate),
+            dt2 = new Date();
+        this.timeDiff = dt1.getTime() - dt2.getTime();
 
-        this.timeDiff = utc1 - utc2;
-        this.startingTime = new Date(this.task.dueDate).getTime() / 1000;
         this.timer = Observable.timer(0, this.period);
         this.sub = this.timer.subscribe(tick => this.report = this.convertToCountdown(this.timeDiff - (tick * 1000)));
     }
@@ -41,12 +37,14 @@ export class TaskComponent implements OnInit, OnDestroy {
     }
 
     convertToCountdown(ms: number): string {
-        let baseDay = 24 * 60 * 60 * 1000, // 24hours * 60minutes * 60seconds * 1000milliseconds
-            baseHour = 60 * 60 * 1000, // 60minutes * 60seconds * 1000milliseconds
-            days = Math.floor(ms / baseDay),
-            hours = Math.round((ms - days * baseDay) / baseHour),
-            minutes = Math.round((ms - days * baseDay - hours * baseHour) / 60000);
+        let seconds = Math.floor((ms / 1000) % 60),
+            minutes = Math.floor((ms / 1000 / 60) % 60),
+            hours = Math.floor((ms / (1000 * 60 * 60)) % 24),
+            days = Math.floor(ms / (1000 * 60 * 60 * 24));
 
-        return [days, hours, minutes].join(':');
+        return days.toString() + ' days: ' +
+            hours.toString() + ' hours: ' +
+            minutes.toString() + ' minutes: ' +
+            seconds.toString() + ' seconds';
     }
 }
